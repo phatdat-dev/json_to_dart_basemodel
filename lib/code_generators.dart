5979450534a2e24fff8a4c87e9e571a8621e1b44
wwwwww3q <phatdatfbi@gmail.com>
2022-09-07 20:03:18 +0700

    init

---
+++ lib/code_generators.dart
@@ -0,0 +1,19 @@
+library code_generators;
+
+//dart pub run build_runner build
+import 'package:build/build.dart';
+import 'package:code_generators/src/blog_homepage_builder.dart';
+import 'package:code_generators/src/copy_builder.dart';
+import 'package:code_generators/src/markdown_to_html.dart';
+
+Builder copyBuilder(BuilderOptions options) => CopyBuilder();
+
+Builder markdownToHtmlBuilder(BuilderOptions options) => MarkdownToHtmlBuilder(options);
+
+Builder blogHomepageBuilder(BuilderOptions options) => BlogHomepageBuilder();
+
+PostProcessBuilder cleanupUnneededFiles(BuilderOptions options) => FileDeletingBuilder([
+      '.md',
+      '.yaml',
+      '.lock',
+    ]);
